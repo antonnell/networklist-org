0216457df526272652e8a0b49de1f4d43c6d69ff
Robin Wen <blockxyz@gmail.com>
2022-03-06 11:30:56 +0800

    Remove multichain

--- pages/index.js
+++ pages/index.js
@@ -193,7 +193,6 @@ function Home({ changeTheme, theme }) {
               <Header changeTheme={ changeTheme } />
             </div>
             <div className={ classes.cardsContainer }>
-              { hideMultichain === '0' && <MultiChain closeMultichain={ closeMultichain } /> }
               {
                 data && data.filter((chain) => {
                   if(search === '') {
